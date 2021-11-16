e8687c5dcc6238cd1398d05cbd6b5fcdc278651b
drumil2407 <78135386+drumil2407@users.noreply.github.com>
2021-11-16 01:03:33 -0500

    First Commit
    Add first four classes
    - abstract class AbstractEntertaiment
    - class Movies
    - class TVseries
    - class UpcomingMoviesAndTvShows

---
+++ Entertainment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Entertainment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
